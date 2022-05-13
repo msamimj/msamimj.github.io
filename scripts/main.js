b55fe587a33db6639e70740a1885e10b078858ba
Sammim <msamimj@gmail.com>
2022-05-14 00:29:52 +0530

    added no-javascript fallback

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,11 @@ let sectionsSwitch = document.getElementById("sections-switch");
 let sectionsList = document.getElementsByClassName("section");
 let content = document.getElementById("content")
 
+
 let currentSection = 0;
 let sectionsCount = sectionsList.length;
+
+document.body.classList.add("js-enabled");
 
 sectionsSwitch.addEventListener("click", () => {
     content.scroll({
