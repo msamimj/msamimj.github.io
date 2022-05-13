632ffd377b09d9618492b7adb8bd4ac9332e89d1
Sammim <msamimj@gmail.com>
2022-05-14 02:17:03 +0530

    added dummy content and content styling with description

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
-let sectionsSwitch = document.getElementById("sections-switch");
+let sectionsSwitch = document.getElementById("sections-switch-wrapper");
+let sectionsSwitchSectionsList = document.getElementsByClassName("sections-switch-section");
 let sectionsList = document.getElementsByClassName("section");
 let content = document.getElementById("content")
 
@@ -9,7 +10,9 @@ let sectionsCount = sectionsList.length;
 document.body.classList.add("js-enabled");
 
 sectionsSwitch.addEventListener("click", () => {
+    sectionsSwitchSectionsList[currentSection].classList.remove("enabled");
     currentSection = (++currentSection)%sectionsCount;
+    sectionsSwitchSectionsList[currentSection].classList.add("enabled");
     sectionWidth = sectionsList[0].clientWidth;
     content.scroll({
         left: sectionWidth*currentSection,
