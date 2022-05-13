a7ad477ab7f64cba1859b4c8d886c7c5105dd2b3
Sammim <msamimj@gmail.com>
2022-05-13 23:48:30 +0530

    basic layout using flexbox

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+let sectionsSwitch = document.getElementById("sections-switch");
+let sectionsList = document.getElementsByClassName("section");
+let content = document.getElementById("content")
+
+let currentSection = 0;
+let sectionsCount = sectionsList.length;
+
+sectionsSwitch.addEventListener("click", () => {
+    content.scroll({
+        top: 0,
+        left: (sectionsList[0].clientWidth*((++currentSection)%sectionsCount)),
+        behavior: 'smooth'
+    });
+})
